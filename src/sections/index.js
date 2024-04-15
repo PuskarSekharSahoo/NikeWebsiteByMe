78d71726abbc0119bb7361760652eba3bc53ce2f
PuskarSekharSahoo <puskarsahoo4209@gmail.com>
2024-04-15 14:01:06 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SuperQuality,
+    SpecialOffers,
+    Footer,
+    CustomerReviews
+
+};
